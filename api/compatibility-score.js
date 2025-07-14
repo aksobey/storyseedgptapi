4e81691fd9ed5376d8d0006f47a11857c2f7eff5
Kent Sobey <ksobey@yahoo.com>
2025-07-14 16:16:52 +0800

    CORS fix 100

--- api/compatibility-score.js
+++ api/compatibility-score.js
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from 'openai';
 export default async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
