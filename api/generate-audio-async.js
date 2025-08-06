a1512df069e337a8ddfedc40e2ee3ee24ffdf43b
Kent Sobey <ksobey@yahoo.com>
2025-08-06 21:21:23 +0800

    Fix CORS headers on TTS endpoints to match working endpoints

--- api/generate-audio-async.js
+++ api/generate-audio-async.js
@@ -43,13 +43,14 @@ function initializeFirebase() {
 }
 
 export default async function handler(req, res) {
-  // CORS headers
+  // CORS headers - match working endpoints exactly
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    res.status(200).end();
+    return;
   }
 
   if (req.method !== 'POST') {
